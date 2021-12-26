bf0284b7dc0a65a9549c10fe73c06a135f7b2ec9
Gabriel Leite <gabrielholeite@gmail.com>
2021-12-26 15:36:33 +0000

    Repetition structure and when keyword

--- src/main.kt
+++ src/main.kt
@@ -1,13 +1,30 @@
 fun main() {
     println("Seja bem vindo ao Byte Bank")
 
-    val titular = "Gabriel"
-    val accountNumber = 1234
-    var balance = 0.0
-    balance += 100.0
+    for(i in 1..3) {
+        val titular = "Joe $i"
+        val accountNumber = 1234 + i
+        var balance = 0.0 + i
+        balance += 100.0
 
-    println("titular: $titular")
-    println("numero da conta: $accountNumber")
-    println("saldo: $balance")
+        println("titular: $titular")
+        println("numero da conta: $accountNumber")
+        println("saldo: $balance")
 
+        validateBalance(balance)
+    }
+
+    for(i in 5 downTo 1 step 2) {
+        println(i)
+    }
+
+
+}
+
+fun validateBalance(balance: Double) {
+    when {
+        balance > 0.0 -> println("saldo é positivo")
+        balance == 0.0 -> println("sem saldo")
+        else -> println("saldo é negativo")
+    }
 }
