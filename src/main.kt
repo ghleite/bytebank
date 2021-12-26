f31db95b5035835398884374d4846c232f5e4fa9
Gabriel Leite <gabrielholeite@gmail.com>
2021-12-26 15:07:03 +0000

    Adding bytebank files

---
+++ src/main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    println("Seja bem vindo ao Byte Bank")
+
+    val titular = "Gabriel"
+    val accountNumber = 1234
+    var balance = 0.0
+    balance += 100.0
+
+    println("titular: $titular")
+    println("numero da conta: $accountNumber")
+    println("saldo: $balance")
+
+}
